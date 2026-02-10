ed9a374b4a83c1c5ed5a1a0719abca41334b2036
Gaston Tortul <tortulgaston7@gmail.com>
2026-02-10 18:02:30 -0300

    Proyecto finalizado

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,105 +1,96 @@
-import { useState, useEffect } from "react"
-import { Button } from "./ui/Button"
-import {Card} from './ui/Card'
-import { Search } from "./ui/Search"
-import {v4 as uuid} from "uuid"
+import { useState, useEffect } from "react";
+import { Button } from "./ui/Button";
+import { Card } from "./ui/Card";
+import { Search } from "./ui/Search";
+import { v4 as uuid } from "uuid";
 
 export const Main = () => {
+  const [page, setPage] = useState(1);
+  const [genders, setGenders] = useState([]);
+  const [selectedGender, setSelectedGender] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const config = {
+    headers: {
+      authorization: `Bearer ${import.meta.env.PUBLIC_API_TOKEN}`,
+      "content-type": "application/json",
+    },
+  };
+  const getGenders = async () => {
+    try {
+      const url = "https://api.themoviedb.org/3/genre/movie/list?language=es";
+      const req = await fetch(url, config);
+      const res = await req.json();
 
-    const [page, setPage] = useState(1)
-    const [genders, setGenders] = useState([])
-    const [selectedGender, setSelectedGender] = useState("")
-    const [movies, setMovies] = useState ([])
-    const [filteredMovies, setFilteredMovies] = useState ([])
-    const [loading, setLoading] = useState (false)
-    const config = {
-        headers:{
-            authorization: `Bearer ${import.meta.env.PUBLIC_API_TOKEN}`,
-            'content-type' : 'application/json'
-        }
+      if (req.status === 200) {
+        setGenders(res.genres);
+      }
+    } catch (err) {
+      console.error(err);
     }
-    const getGenders = async ()=>{
-        try{
-            const url = "https://api.themoviedb.org/3/genre/movie/list?language=es"
-            const req = await fetch(url, config)
-            const res = await req.json()
+  };
+  const addPage = () => {
+    setPage(page + 1);
+  };
+  const getMovies = async () => {
+    try {
+      setLoading(true);
+      const url = `https://api.themoviedb.org/3/movie/popular?language=es-ES&page=${page}`;
 
-            if (req.status === 200){
-                setGenders(res.genres)
-            }
-            }catch(err){
-                console.error(err)
-            }
-        }
-    const addPage = () => {
-        setPage (page + 1)
+      const req = await fetch(url, config);
+      if (req.status === 200) {
+        const res = await req.json();
+        setMovies([...movies, ...res.results]);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
-    const getMovies = async ()=>{
-        try{   
-            setLoading(true)
-            const url = `https://api.themoviedb.org/3/movie/popular?language=es-ES&page=${page}`
-            
-            const req = await fetch(url, config)
-            if (req.status === 200){
-                const res = await req.json()
-                console.log (res)
-                setMovies([movies.concat(res.results)])
-            }
-        }
-         catch(err){
-            console.error(err)
-        } finally{
-            setLoading(false)
-        } 
-        }
+  };
 
-
-    const filterMovies= () => {
-
-        if(selectedGender === ""){
-            setFilteredMovies(movies)
-        }else{
-            const fm= movies.filter(movie=>{
-              if(movie?.genre_ids.includes(Number(selectedGender))){
-                return movie
-              }  
-            })
-            setFilteredMovies(fm)
+  const filterMovies = () => {
+    if (selectedGender === "") {
+      setFilteredMovies(movies);
+    } else {
+      const fm = movies.filter((movie) => {
+        if (movie?.genre_ids.includes(Number(selectedGender))) {
+          return movie;
         }
-        console.log (filteredMovies)
-        console.log (movies)
+      });
+      setFilteredMovies(fm);
     }
-    useEffect(()=>{
-        getGenders()
-    }, [])
+  };
+  useEffect(() => {
+    getGenders();
+  }, []);
 
-    useEffect(() =>{
-        getMovies()
-    }, [page])
+  useEffect(() => {
+    getMovies();
+  }, [page]);
 
-    useEffect(()=>{
-        filterMovies()
-    }, [movies, selectedGender])
+  useEffect(() => {
+    filterMovies();
+  }, [movies, selectedGender]);
 
-    
-    return <div className="contenedor main">
-    <Search 
-        selectedGender = {selectedGender}
-        setSelectedGender = {setSelectedGender} 
-        genders = {genders}
-     />
-    <section className="main_cards">
-
-        {
-            movies.map((movie) => {
-                return <Card key={uuid()} movie={movie} />
-            })
-        }
-        
-    </section>
-    {
-        loading ? <p>Cargando...</p> : <Button onClick = {addPage}>Ver Más</Button>
-    }
-    
+  return (
+    <div className="contenedor main">
+      <Search
+        selectedGender={selectedGender}
+        setSelectedGender={setSelectedGender}
+        genders={genders}
+      />
+      <section className="main__cards">
+        {filteredMovies.map((movie) => {
+          return <Card key={uuid()} movie={movie} />;
+        })}
+      </section>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <Button onClick={addPage}>Ver MÃ¡s</Button>
+      )}
     </div>
-} 
+  );
+};
